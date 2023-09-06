7209e1b0e8d0aae6e8ccb79260f38c816c809b29
John Quiroz <bayron.quiroz@udea.edu.co>
2023-09-06 12:33:27 -0500

    El editor del héroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+// import { RouterModule } from '@angular/router';
+// import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+// import { TopBarComponent } from './top-bar/top-bar.component';
+// import { ProductListComponent } from './product-list/product-list.component';
+// import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    FormsModule,
+    // ReactiveFormsModule,
+    // RouterModule.forRoot([
+    //   { path: '', component: ProductListComponent },
+    // ])
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent
+    // TopBarComponent,
+    // ProductListComponent,
+    // ProductAlertsComponent,
+    HeroesComponent
   ],
   bootstrap: [
     AppComponent
